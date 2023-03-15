1de89bd8014d4f30bdd30f9d474d9311fcd3db89
mikljohsn <mxjohansen@hotmail.dk>
2023-03-15 10:07:10 +0100

    Det bedste commit 2

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+       boolean running = true;
 
 
         System.out.println("Hey");
         //Her er en kommentar i koden ;)
         //Ny kommentar
         Scanner in = new Scanner(System.in);
-        System.out.println("Brøndby eller fck?");
-        String svar = in.nextLine();
-        if (svar.equalsIgnoreCase("Brøndby")){
-            System.out.println("forkert");
-        } else if (svar.equalsIgnoreCase("Fck")) {
-            System.out.println("korrekt");
+        do {
+            System.out.println("brøndby eller fck");
+            String svar = in.nextLine();
+            if (svar.equalsIgnoreCase("Brøndby")) {
+                System.out.println("forkert");
+            } else if (svar.equalsIgnoreCase("Fck")) {
+                System.out.println("korrekt");
+                running = false;
 
-        } else {
-            System.out.println("Skriv lige fck");
+            } else {
+                System.out.println("Skriv lige fck");
+            }
         }
+        while (running);
     }
 }
